3ed135729b6693e2c8ed898d0d76cba0be59ed8e
Douglas Schneider <dlschneider@proton.me>
2026-03-12 23:49:22 -0300

    Removida a instalação do Neo-tree e utilizada a que já vem no Kickstart

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,15 +5,4 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      'nvim-tree/nvim-web-devicons',
-    },
-    lazy = false,
-  },
-}
+return {}
