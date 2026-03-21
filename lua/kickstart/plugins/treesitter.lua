9ad05fa204da9d6296ffadbaef7b204444ad1598
Douglas Schneider <dlschneider@proton.me>
2026-03-20 23:12:09 -0300

    Adiciona a Linguagem HCL e do Terraform ao Treesitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -5,21 +5,17 @@ return { -- Highlight, edit, and navigate code
   branch = 'main',
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
   config = function()
-    local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+    local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'terraform', 'hcl' }
     require('nvim-treesitter').install(parsers)
 
     vim.api.nvim_create_autocmd('FileType', {
       callback = function(args)
         local buf, filetype = args.buf, args.match
         local language = vim.treesitter.language.get_lang(filetype)
-        if not language then
-          return
-        end
+        if not language then return end
 
         -- check if parser exists and load it
-        if not vim.treesitter.language.add(language) then
-          return
-        end
+        if not vim.treesitter.language.add(language) then return end
 
         -- enables syntax highlighting and other treesitter features
         vim.treesitter.start(buf, language)
